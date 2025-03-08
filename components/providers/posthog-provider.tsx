8aba1a582ac477206dc6f040bc28cf7fbf8a24a1
Deepak Mardi <deepakmardi22@gmail.com>
2025-03-08 20:11:26 +0530

    posthog update

--- components/providers/posthog-provider.tsx
+++ components/providers/posthog-provider.tsx
@@ -10,7 +10,11 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
     loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug();
+      if (process.env.NODE_ENV === "development") {
+        posthog.debug();
+      } else {
+        posthog.opt_in_capturing();
+      }
     },
     capture_pageview: false, // Disable automatic pageview capture, as we'll handle this manually
   });
